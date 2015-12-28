ecec82673aac6722d564681b714cc34763616183
dabwax <luizhrqas@gmail.com>
2015-12-28 15:10:47 -0200

    bugfix facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, "485103155007447", "c34da6e152e667b4992669b205dfac39", :scope => "email,relationship_details"
+	provider :facebook, "485103155007447", "c34da6e152e667b4992669b205dfac39", :scope => "email"
 end
