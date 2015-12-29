96a3bb47d69ef3d8674955020a60e13dd7636dbb
dabwax <luizhrqas@gmail.com>
2015-12-29 11:30:16 -0200

    add new shopping list ui init

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -2,4 +2,8 @@ class ShoppingController < ApplicationController
 
   def index
   end
+
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'auth/failure', to: redirect('/')
   get 'auth/signout', to: 'sessions#destroy', as: 'signout'
 
+  # Shopping List
   get 'shopping-list', to: 'shopping#index', as: 'shopping_list'
+  get 'shopping-list/add-new', to: 'shopping#create', as: 'add_new_list'
 
   root to: 'welcome#index'
 end
